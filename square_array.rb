65e1ef299667f2ca08acb5a0288da6e963f137aa
Patrick Lynch <pajlynch@gmail.com>
2021-04-02 21:38:29 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,7 @@
 def square_array(array)
-  array = ["1", "2", "3"]
-  array.each do |number|
-    puts "#{number}**2"
-  end
+ new_array = []
+ array.each do |integer|
+  new_array << integer ** 2
+ end
+ return new_array
 end
